4c49c9048710df4e57dfb57f42ec01b1cb978a84
Atul12764 <42302907+Atul12764@users.noreply.github.com>
2021-04-03 12:48:43 +0530

    Initial commit

---
+++ src/main/java/com/checkbeep/config/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.checkbeep.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/v1/address/**").hasRole("USER")
+                .antMatchers("/api/v1/orderDetails/**").hasRole("USER")
+                .antMatchers("/api/v1/orders/**").hasRole("USER")
+                .antMatchers("/api/v1/payment/**").hasRole("USER")
+                .antMatchers("/api/v1").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
